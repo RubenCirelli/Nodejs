c02e4864fac06f329fe2c5ec5696e4327bf90a91
Ruben <rubencirelli4@gmail.com>
2024-02-08 22:35:19 +0100

    Command-line-art

---
+++ message.js
@@ -0,0 +1,10 @@
+var figlet = require("figlet");
+
+figlet("Ciao a tutti!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
