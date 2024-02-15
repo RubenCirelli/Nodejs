369d2b4fa5241debb23216c8f467988548f5904f
Ruben <rubencirelli4@gmail.com>
2024-02-15 16:08:07 +0100

    Upload-files

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import { getAll, getOneByID, create, updateById, deleteById, createImage} from "./controller/planets.js"
+import multer from "multer";
 
 
-import { getAll, getOneByID, create, updateById, deleteById} from "./controller/planets.js"
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) =>{
+    cb(null, "./uploads")
+  },
+  filename: (req, file, cb) =>{
+    cb(null, file.originalname)
+  },
+})
+const upload = multer({storage})
+
 dotenv.config();
 
 const app = express();
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("static"));
 
 app.use(morgan("dev"));
 app.use(express.json())
@@ -21,6 +35,8 @@ app.use(express.json())
  app.put("/api/planets/:id", updateById)
 
 app.delete('/api/planets/:id', deleteById);
+
+app.post("/api/planets/:id/image",upload.single( "image"),  createImage)
 // Start the server
 const port = process.env.PORT || 3000;
 
