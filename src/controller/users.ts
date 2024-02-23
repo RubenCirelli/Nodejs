a23283402a2ead0021bffee2163749a8483dae3d
Ruben <rubencirelli4@gmail.com>
2024-02-23 17:09:55 +0100

    JWT Auth part 1

---
+++ src/controller/users.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { db } from "./../db.js";
+import jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const logIn = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  const user = await db.one(`SELECT * FROM users WHERE username=$1`, username);
+
+  if (user && user.password === password) {
+    const payload = {
+      id: user.id,
+      username,
+    };
+    const { SECRET = "" } = process.env;
+    const token = jwt.sign(payload, SECRET);
+    await db.none ("UPDATE users SET token=$2 WHERE id=$1", [user.id, token]);
+    return res.status(200).send({ id: user.id, username, token });
+    
+  } else {
+    res.status(400).json({ message: "Invalid username or password" });
+  }
+};
+
+export { logIn };
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import { getAll, getOneByID, create, updateById, deleteById, createImage} from "./controller/planets.js"
+import {
+  getAll,
+  getOneByID,
+  create,
+  updateById,
+  deleteById,
+  createImage,
+} from "./controller/planets.js";
 import multer from "multer";
-
-
+import { logIn } from "./controller/users.js";
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) =>{
-    cb(null, "./uploads")
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
   },
-  filename: (req, file, cb) =>{
-    cb(null, file.originalname)
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
   },
-})
-const upload = multer({storage})
+});
+const upload = multer({ storage });
 
 dotenv.config();
 
@@ -23,20 +29,20 @@ app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("static"));
 
 app.use(morgan("dev"));
-app.use(express.json())
+app.use(express.json());
 
+app.get("/api/planets", getAll);
 
- app.get("/api/planets", getAll)
+app.get("/api/planets/:id", getOneByID);
 
- app.get("/api/planets/:id", getOneByID)
+app.post("/api/planets", create);
 
- app.post("/api/planets", create)
+app.put("/api/planets/:id", updateById);
 
- app.put("/api/planets/:id", updateById)
+app.delete("/api/planets/:id", deleteById);
 
-app.delete('/api/planets/:id', deleteById);
-
-app.post("/api/planets/:id/image",upload.single( "image"),  createImage)
+app.post("/api/planets/:id/image", upload.single("image"), createImage);
+app.post("/api/users/login", logIn);
 // Start the server
 const port = process.env.PORT || 3000;
 
